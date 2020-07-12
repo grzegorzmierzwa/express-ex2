626de3cae257b2e9b0b6ed65cbcee9110b757f98
Grzegorz Mierzwa <mierzwagrzegorz@gmail.com>
2020-07-12 23:52:56 +0200

    add hbs

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const path = require('path');
 
 const app = express();
 
+const hbs = require('express-handlebars');
+
+app.engine('hbs', hbs());
+app.set('view engine', 'hbs');
+
 
 app.use((req, res, next) => {
     res.show = (name) => {
@@ -16,17 +21,21 @@ app.use('/user', (req, res, next) => {
     next();
 });
 
+app.get('/hello/:name', (req, res) => {
+    res.render('hello', { layout: false, name: req.params.name });
+});
+
 app.get('/', (req, res) => {
-    res.show('home.html');
+    res.render('home', { layout: false });
 });
 
 app.get('/about', (req, res) => {
-    res.show('about.html');
+    res.render('about', { layout: false });
 });
 
 app.get('/404.png', (req, res) => {
     res.sendFile(path.join(__dirname, '/404.png'));
-  });
+});
 
 app.use((req, res) => {
     res.status(404).show('404.html');
